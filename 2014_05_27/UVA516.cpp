1600d536d0d962ebb2fcbb8c8001db7e4d81ceb3
MKE0108 <mickyt711ss@gmail.com>
2022-06-30 20:03:30 +0800

    ss

--- 2014_05_27/UVA516.cpp
+++ 2014_05_27/UVA516.cpp
@@ -1,1 +1,19 @@
 #include<iostream>
+#include<cmath>
+#include<sstream>
+#include<string>
+#include<vector>
+using namespace std;
+int main() {
+	string s;
+	while (getline(cin,s)) {
+		stringstream ss(s);
+		int temp;
+		vector<int> seq;
+		while (ss >> temp) {
+			seq.push_back(temp);
+		}
+		unsigned long long x;
+
+	}
+}
